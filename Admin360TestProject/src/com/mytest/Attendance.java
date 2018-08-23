f663ee64f052427682c7a2d56eaec7c8d498ea47
vaishalibachhav <vaishub94@gmail.com>
2018-08-23 16:25:53 +0530

    new

--- Admin360TestProject/src/com/mytest/Attendance.java
+++ Admin360TestProject/src/com/mytest/Attendance.java
@@ -16,7 +16,7 @@ import org.testng.annotations.Test;
 	  @Test
 	  public void Add_Course() throws InterruptedException 
 	  {
-		  Thread.sleep(200);
+		  Thread.sleep(100);
 		  driver.findElement(By.xpath("//*[@id=\"nav-state-toggle\"]/i")).click();
 		  driver.findElement(By.xpath("//*[@id=\"2\"]/li[1]/a")).click();
 		  driver.findElement(By.xpath("//*[@id=\"btn-new-course\"]")).click();
